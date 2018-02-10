b34a8ef7d04d8f392786dbc22e21950dae3edda3
Greg Church <greggypc@users.noreply.github.com>
2018-02-10 14:43:05 -0600

    begin setup files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burger (burger_name, devoured) VALUES ('Dubbel Cheese Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Triple Meat Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Veggie Burger', false);
---
+++ server.js
@@ -0,0 +1,5 @@
+//burger
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
