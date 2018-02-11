668b56f7dc31a088f02c70be40fe1f9a0c849634
Greg Church <greggypc@users.noreply.github.com>
2018-02-11 12:47:47 -0600

    handlebars lay out

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".devour-burger").on("click", function(event) {
+    var id = $(this).data("id");
+    var isDevoured = $(this).data("isDevoured");
+
+    var devouredState = {
+      devour: isDevoured
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(
+      function() {
+        console.log("burger has been devoured");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burg").val().trim(),
+      // sleepy: $("[name=sleepy]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+});
